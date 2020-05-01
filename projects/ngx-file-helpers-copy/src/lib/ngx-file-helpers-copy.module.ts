aebf4afe93ae217169a9eb04386b4bc97dafd1db
paxmylona <mcraig@zsconsultants.com>
2020-05-01 15:30:31 -0400

    updated name

---
+++ projects/ngx-file-helpers-copy/src/lib/ngx-file-helpers-copy.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { FileDropzoneDirective } from './file-dropzone.directive';
+import { FilePickerDirective } from './file-picker.directive';
+
+@NgModule({
+  declarations: [FileDropzoneDirective, FilePickerDirective],
+  imports: [],
+  exports: [FileDropzoneDirective, FilePickerDirective]
+})
+export class NgxFileHelpersCopyModule {}
